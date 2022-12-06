c14d2b17960b3757eede56d5f12ba6f30e4126e1
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2022-12-06 08:10:46 -0500

    Validaciones de PQRS

--- Ingenio.PortalWebExterno/Models/Formato.cs
+++ Ingenio.PortalWebExterno/Models/Formato.cs
@@ -8,27 +8,37 @@ namespace Ingenio.PortalWebExterno.Models
 {
     public class Formato
     {
-        [Required(ErrorMessage = "Title is required")]
+        [Required(ErrorMessage = "La cedula es obligatoria")]
+        [Display(Name = "Cedula*")]
         public decimal Cedula { get; set; }
+
+        [Required(ErrorMessage = "Los nombres son obligatorio")]
+        [Display(Name = "Nombre*")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "Los apellidos son obligatorio")]
+        [Display(Name = "Apellido*")]
         public string Apellido { get; set; }
         public Asociados Asociado { get; set; }
+        public Agencias Agencia { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [Display(Name = "Correo*")]
         public string Correo { get; set; }
         public string Telefono { get; set; }
         public DateTime Fecha { get; set; }
+        
+        [StringLength(300, ErrorMessage = "Escribe maximo 300 palabras")]
         public string Descripcion { get; set; }
 
         [Display(Name = "Autorizacion*")]
         [Range(typeof(bool), "true", "true", ErrorMessage = "Tiene que autorizar")]
         public bool Autorizacion { get; set; }
-
-        //public formfiles Archivo { get; set; }
     }
 
     public enum Asociados
     {
-        si,
-        no
+        Si,
+        No
     }
     public enum Agencias
     {
